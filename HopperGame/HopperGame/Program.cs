b32e482c312821297b39e57892f55e76851fed16
D1m0nd <d1m0hkrasav4ik@gmail.com>
2020-08-26 12:23:44 +0300

    123

--- HopperGame/HopperGame/Program.cs
+++ HopperGame/HopperGame/Program.cs
@@ -1,12 +1,47 @@
 ﻿using System;
+using System.IO;
+using System.Security.Cryptography.X509Certificates;
+using System.Text.Json;
+using System.Threading.Tasks;
+using System.Collections.Generic;
 
 namespace HopperGame
-{
+{// Root myDeserializedClass = JsonConvert.DeserializeObject<Root>(myJsonResponse); 
+    public class Player
+    {
+        public string[] gender { get; set; }
+        public string[] stage_dev { get; set; }
+        public string[] Body { get; set; }
+    }
+
+
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // чтение данных
+            FileStream fs = new FileStream("D:/Рабочий стол/Git_proj/HopperGame/HopperGame/player.json", FileMode.OpenOrCreate);
+            Player restoredPlayer = await JsonSerializer.DeserializeAsync<Player>(fs);
+            Console.WriteLine("Все ОК");
+
+            string gender_player = PropertyRandStr(restoredPlayer.gender);
+            string stage_dev_player = PropertyRandStr(restoredPlayer.stage_dev);
+            string body_player = PropertyRandStr(restoredPlayer.Body);
+
+
+
+            
+            
+            
         }
+
+        //Генерация свойства gender
+        static string PropertyRandStr(string[] obj)
+        {
+            Random rand = new Random();
+            return obj[rand.Next(obj.Length)];
+        }
+
+        
     }
 }
