4543c17aaca6e15dcc44b75e80eb935936eaa2e1
D1m0nd <d1m0hkrasav4ik@gmail.com>
2020-09-26 12:46:38 +0300

    Add property class Hopper

--- HopperGame/HopperGame/Hopper.cs
+++ HopperGame/HopperGame/Hopper.cs
@@ -1,10 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Runtime.Serialization;
 using System.Text;
 
 namespace HopperGame
 {
     class Hopper
     {
+        //Площадь  бункера в метрах квадратных
+        public int Area;
+        //Количество комнат
+        public int CountRoom;
+        //Расположение
+        [DataMember]
+        public string Location;
+        //Вещи расположенные в бункере
+        [DataMember]
+        public string Things;
+
     }
 }
