f87d32ef6f32cbd613c0ebfeaac6c98229ae4357
D1m0nd <d1m0hkrasav4ik@gmail.com>
2020-09-26 12:36:46 +0300

    Add class disaster and hopper

---
+++ HopperGame/HopperGame/Disaster.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using System.Text;
+
+namespace HopperGame
+{
+
+    class Disaster
+    {
+        //Описание катострофы
+        [DataMember]
+        public string Description;
+        //Дополнительное описание катастрофы
+        [DataMember]
+        public string SubDescription;
+        //Катоклзимы
+        [DataMember]
+        public string Cataclysm;
+        //Остаток насления
+        public int RemainderPopulation;
+        //Остаток воды
+        public int RemainderWater;
+      
+    }
+}
